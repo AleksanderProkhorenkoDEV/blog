e349248be2e823be551bd38b74971d3c81920186
aleksander <aprokhorenkodev@gmail.com>
2026-02-19 18:58:11 +0100

    FRONT | PAGE | feat: craeate a simple form with useAction to test the validation

--- front/app/auth/register/page.tsx
+++ front/app/auth/register/page.tsx
@@ -1,5 +1,33 @@
-export default function Register(){
-    return ( 
-        <h1>Register screen</h1>
+'use client'
+
+import { userRegistration } from "@/actions/auth";
+import { initStateRegisterForm } from "@/types/auth";
+import { useActionState } from "react";
+
+export default function Register() {
+
+    const initialState: initStateRegisterForm = {
+        success: false,
+        errors: {}
+    }
+
+    const [state, formAction, pending] = useActionState(userRegistration, initialState)
+    console.log(state.errors?.email?.[0]);
+    
+    return (
+        <>
+            <h1>Register page</h1>
+            <form action={formAction}>
+                <label htmlFor="name">name</label>
+                <input type="text" id="name" name="name" />
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" />
+                <label htmlFor="password" >Password</label>
+                <input type="password" id="password" name="password" />
+                <label htmlFor="password" >Password confirmation</label>
+                <input type="password" id="password" name="passwordConfirmation" />
+                <button type="submit" disabled={pending}>Submit</button>
+            </form>
+        </>
     )
 }
