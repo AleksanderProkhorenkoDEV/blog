08077bcc1b8622bf7f4ee72644679ea2a184d52c
aleksander <aprokhorenkodev@gmail.com>
2026-02-21 19:31:44 +0100

    FRONT | AUTH | feat: add logic to sing up and reditect to confirm email

--- front/actions/auth.ts
+++ front/actions/auth.ts
@@ -1,19 +1,42 @@
 'use server'
 
 import { initStateRegisterForm } from "@/types/auth"
-import { regiserSchema } from "../app/schemas/auth"
+import { createClient } from "@/lib/supabase/server"
+import { redirect } from "next/navigation"
+import { registerSchema } from "@/schemas"
 import z from "zod"
 
 export const userRegistration = async (prevState: initStateRegisterForm, formData: FormData): Promise<initStateRegisterForm> => {
 
-    const validateFields = regiserSchema.safeParse(Object.fromEntries(formData.entries()))
+    const validateFields = registerSchema.safeParse(Object.fromEntries(formData.entries()))
 
     if (!validateFields.success) {
         return {
             success: false,
-            errors: z.flattenError(validateFields.error).fieldErrors
+            inputErrors: z.flattenError(validateFields.error).fieldErrors,
+            formData: {
+                name: formData.get("name") as string,
+                email: formData.get("email") as string,
+            }
         }
     }
+
+    const supabase = await createClient()
+
+    const { error } = await supabase.auth.signUp({
+        email: validateFields.data.email,
+        password: validateFields.data.password,
+        options: {
+            data: {
+                name: validateFields.data.name,
+            },
+            emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/posts`
+        }
+    })
+
+    if (error) return { success: false, formError: error.message }
+
+    redirect("/auth/register/check-email")
 
     return { success: true }
 }
