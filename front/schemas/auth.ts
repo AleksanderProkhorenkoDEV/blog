bd23f007d63d9e5493078b902e8516a6a4775205
aleksander <aprokhorenkodev@gmail.com>
2026-02-21 19:29:39 +0100

    FRONT | ZOD | refactor: move the folder out of app router

--- front/schemas/auth.ts
+++ front/schemas/auth.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
-export const regiserSchema = z.object({
+export const registerSchema = z.object({
     name: z.string({ error: "El nombre debe ser válido, no puede estar vacio." }).min(2, { error: "El nombre tiene que tener mínimo dos carácteres." }),
     email: z.email({ error: "El email tiene que tener un formato válido: ejemplo@dominio.com" }),
     password: z.string({ error: "El campo contraseña debe estar relleno" }).regex(/^.{8,}$/, { error: "Tiene que ser una combinación de ocho números, letras y carácteres" }),
