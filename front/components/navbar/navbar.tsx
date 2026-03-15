e0c767f3528342607653a88f8f2dcb6c1c90696a
Aleksander <aprokhorenkodev@gmail.com>
2026-03-15 10:53:06 +0100

    FRONT | COMPONENT |  feat: add items to the menu and change the name of the component

--- front/components/navbar/navbar.tsx
+++ front/components/navbar/navbar.tsx
@@ -2,13 +2,26 @@
 
 import {
     NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuList,
 } from "@/components/ui/navigation-menu"
+import { NavBarLink } from "./Link"
 
-
-export const NavBar = () => {
+export const CustomNavBar = () => {
     return (
         <NavigationMenu>
-            
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <NavBarLink href="/">{`>_ ~/init `}</NavBarLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavBarLink href="/">Inicio</NavBarLink>
+                </NavigationMenuItem>
+                
+                <NavigationMenuItem>
+                    <NavBarLink href="/contact">Contact</NavBarLink>
+                </NavigationMenuItem>
+            </NavigationMenuList>
         </NavigationMenu>
     )
 }
