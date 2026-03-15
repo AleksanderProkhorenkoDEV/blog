1743f2a76deeeb3e46891d8cab3ee6911d73cb6f
Aleksander <aprokhorenkodev@gmail.com>
2026-03-15 15:52:03 +0100

    FRONT | COMPONENT | refactor: estar to customize the shadcn

--- front/components/ui/navigation-menu.tsx
+++ front/components/ui/navigation-menu.tsx
@@ -39,7 +39,7 @@ function NavigationMenuList({
     <NavigationMenuPrimitive.List
       data-slot="navigation-menu-list"
       className={cn(
-        "group flex flex-1 list-none items-center justify-center gap-0",
+        "group flex flex-1 list-none items-center justify-center gap-4 border",
         className
       )}
       {...props}
@@ -127,7 +127,7 @@ function NavigationMenuLink({
     <NavigationMenuPrimitive.Link
       data-slot="navigation-menu-link"
       className={cn(
-        "flex items-center gap-2 rounded-lg p-2 text-sm transition-all outline-none hover:bg-muted focus:bg-muted focus-visible:ring-3 focus-visible:ring-ring/50 focus-visible:outline-1 in-data-[slot=navigation-menu-content]:rounded-md data-active:bg-muted/50 data-active:hover:bg-muted data-active:focus:bg-muted [&_svg:not([class*='size-'])]:size-4",
+        "flex items-center gap-2 rounded-lg p-2 text-sm  transition-all outline-none focus:bg-muted focus-visible:ring-3 focus-visible:ring-ring/50 focus-visible:outline-1 in-data-[slot=navigation-menu-content]:rounded-md data-active:bg-muted/25  data-active:focus:bg-muted data-active:text-primary [&_svg:not([class*='size-'])]:size-4",
         className
       )}
       {...props}
