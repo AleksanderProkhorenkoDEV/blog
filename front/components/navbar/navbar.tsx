f2e9b1481114fe3eab565424ce347c00c0d2ee42
Aleksander <aprokhorenkodev@gmail.com>
2026-03-15 15:52:40 +0100

    FRONT | COMPONENT | feat: start to define the header component with navigation

--- front/components/navbar/navbar.tsx
+++ front/components/navbar/navbar.tsx
@@ -9,19 +9,25 @@ import { NavBarLink } from "./Link"
 
 export const CustomNavBar = () => {
     return (
-        <NavigationMenu>
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                    <NavBarLink href="/">{`>_ ~/init `}</NavBarLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavBarLink href="/">Inicio</NavBarLink>
-                </NavigationMenuItem>
-                
-                <NavigationMenuItem>
-                    <NavBarLink href="/contact">Contact</NavBarLink>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
+        <header className="flex items-center justify-between border p-2">
+            <div className="flex gap-2 border">
+                <span className="text-primary">{`>_`}</span><p>~/init</p>
+            </div>
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavBarLink href="/">Inicio</NavBarLink>
+                    </NavigationMenuItem>
+
+                    <NavigationMenuItem>
+                        <NavBarLink href="/blog">Blog</NavBarLink>
+                    </NavigationMenuItem>
+
+                    <NavigationMenuItem>
+                        <NavBarLink href="/contact">Contact</NavBarLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </header>
     )
 }
