64e258beea4df7a1534becbedafeda2aa24791bb
Aleksander <aprokhorenkodev@gmail.com>
2026-03-23 20:03:19 +0100

    FRONT | LAYOUT | refactor: re-organice the layout

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="es" className={`${Konkhmer.className}`}> 
       <body>
-        <CustomNavBar />
+        
         {children}
       </body>
     </html>
