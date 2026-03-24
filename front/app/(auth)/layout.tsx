f7a8c6cea04e8314525d29272f379631cc5f9f9c
Aleksander <aprokhorenkodev@gmail.com>
2026-03-24 20:06:40 +0100

    FRONT | LAYOUT | feat: call the new component and star to customize

--- front/app/(auth)/layout.tsx
+++ front/app/(auth)/layout.tsx
@@ -1,12 +1,14 @@
+import { CurliBraces } from "@/components/elements/curlibraces";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <main className="min-h-screen h-screen grid grid-cols-2 gap-1 ">
-                <section className="border">
-                    <p>Right content</p>
+                <section className="relative">
+                    <CurliBraces position="top-10 left-10">{`{`}</CurliBraces>
+                    <CurliBraces position="bottom-10 right-10">{`}`}</CurliBraces>
                 </section>
-                <section className="border p-4 box-border">
+                <section className="p-4 box-border flex flex-col justify-center items-center gap-4">
                     {children}
                 </section>
             </main>
