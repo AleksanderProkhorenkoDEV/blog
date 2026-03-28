2cd5bcb84b84bc30d8d8f4740c34eadf033eaf3c
aleksander <aprokhorenkodev@gmail.com>
2026-03-28 15:44:01 +0100

    FRONT | LAYOUT | feat: finish the main layout of auth

--- front/app/(auth)/layout.tsx
+++ front/app/(auth)/layout.tsx
@@ -1,11 +1,20 @@
 import { CurliBraces } from "@/components/elements/curlibraces";
+import { jetBrain } from "../fonts/fonts";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <main className="min-h-screen h-screen grid grid-cols-2 gap-1 ">
-                <section className="relative">
+            <main className="min-h-screen grid grid-cols-2 gap-1 ">
+                <section className="relative flex  flex-col items-center justify-center">
                     <CurliBraces position="top-10 left-10">{`{`}</CurliBraces>
+                    <div className="max-w-[80%] mx-auto">
+                        <h1 className={`text-2xl font-medium tracking-tight mb-2 text-foreground/40 ${jetBrain.className}`}>
+                            Todos los caminos llevan a
+                        </h1>
+                        <span className={`inline-block text-3xl font-semibold text-primary ${jetBrain.className}`}>
+                            ~/auth
+                        </span>
+                    </div>
                     <CurliBraces position="bottom-10 right-10">{`}`}</CurliBraces>
                 </section>
                 <section className="p-4 box-border flex flex-col justify-center items-center gap-4">
