5f2e08c074c4360ba87a0bccd73d0bc9b83ce04b
Aleksander <aprokhorenkodev@gmail.com>
2026-04-04 11:45:46 +0200

    FRONT | COMPONENT | feat: change the name of the file

--- front/components/elements/BackgroundDecor.tsx
+++ front/components/elements/BackgroundDecor.tsx
@@ -8,6 +8,8 @@ interface Props {
 
 export const BackgroundDecor = ({ position, children }: Props) => {
     return (
-        <span className={`${jetBrain.className} absolute  ${position} text-9xl text-secondary/55 hover:text-primary/60 ease-in-out transition-all`}>{children}</span>
+        <span className={`${jetBrain.className} absolute  ${position} text-9xl text-secondary/55 hover:text-primary/60 ease-in-out transition-all`}>
+            {children}
+        </span>
     )
 }
