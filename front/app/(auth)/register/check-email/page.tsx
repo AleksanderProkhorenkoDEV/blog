78d58603a360049b8f652a83bf8b56b6ff24e4bb
Aleksander <aprokhorenkodev@gmail.com>
2026-04-04 14:11:53 +0200

    FRONT | PAGE | feat: personalice the verification email

--- front/app/(auth)/register/check-email/page.tsx
+++ front/app/(auth)/register/check-email/page.tsx
@@ -1,5 +1,43 @@
-export default function CheckEmail(){
+'use client'
+
+import { Button } from "@/components/forms/parts/button";
+import { CustomLink } from "@/components/link/customLink";
+import { createClient } from "@/lib/supabase/client";
+import { useState } from "react";
+import { toast } from "sonner";
+
+export default function CheckEmail({ searchParams }: { searchParams: { email: string } }) {
+
+    const [resending, setResending] = useState(false)
+
+    const handleResend = async () => {
+        setResending(true)
+        const supabase = createClient()
+        const { error } = await supabase.auth.resend({
+            type: "signup",
+            email: searchParams.email
+        })
+
+        if (error) {
+            toast.error("Error al reenviar el correo, inténtalo de nuevo")
+        } else {
+            toast.success("Correo reenviado, revisa tu bandeja de entrada")
+        }
+        setResending(false)
+    }
+
     return (
-        <h1>Verify your email</h1>
+        <article className="max-w-lg flex flex-col gap-4">
+            <h1 className="text-2xl font-bold ">¡Ya casi lo tienes!</h1>
+            <p>Hemos enviado un correo al e-mail que nos has proporcionado. Revísalo y verifica tu cuenta.</p>
+            <p className="text-sm">¿No lo encuentras? Revisa tu carpeta de <span className="text-primary font-bold">spam.</span></p>
+            <CustomLink href="/login"><p>Volver al inicio de sesión</p></CustomLink>
+            <Button
+                disabled={resending}
+                onClick={handleResend}
+            >
+                {resending ? "Reenviando..." : "Reenviar correo"}
+            </Button>
+        </article>
     )
 }
