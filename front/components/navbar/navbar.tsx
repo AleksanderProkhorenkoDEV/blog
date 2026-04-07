2bc71f0d59a6db6478f4cb58d1c89648501c640a
Aleksander <aprokhorenkodev@gmail.com>
2026-04-07 17:03:11 +0200

    FRONT | COMPONENT | feat: define the typografi and add border separator

--- front/components/navbar/navbar.tsx
+++ front/components/navbar/navbar.tsx
@@ -6,11 +6,12 @@ import {
     NavigationMenuList,
 } from "@/components/ui/navigation-menu"
 import { NavBarLink } from "./Link"
+import { Konkhmer } from "@/app/fonts/fonts"
 
 export const CustomNavBar = () => {
     return (
-        <header className="flex items-center justify-around p-2">
-            <div className="flex gap-2">
+        <header className="flex items-center justify-around p-2 border-b border-secondary">
+            <div className={`flex gap-2 tracking-widest ${Konkhmer.className}`}>
                 <span className="text-primary">{`>_`}</span><p>~/init</p>
             </div>
             <NavigationMenu>
