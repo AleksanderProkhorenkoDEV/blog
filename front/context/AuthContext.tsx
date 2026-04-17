552341927f910dc3b5be94bb50e5b67731037e15
Aleksander <aprokhorenkodev@gmail.com>
2026-04-17 13:17:38 +0200

    feat: create a auth context

---
+++ front/context/AuthContext.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import React, { createContext, useContext, useState } from "react"
+import { ProfileSummary } from "../types/user"
+
+const AuthContext = createContext<ProfileSummary | null>(null)
+
+interface Props {
+    children: React.ReactNode,
+    initialData: ProfileSummary | null,
+}
+
+export const AuthProvider = ({ children, initialData }: Props) => {
+
+    const [profileState] = useState<ProfileSummary | null>(initialData)
+
+    return (
+        <AuthContext.Provider value={profileState}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export function useAuth(): ProfileSummary {
+    const context = useContext(AuthContext)
+
+    if (!context) {
+        throw new Error("useAuth must be used within AuthProvider")
+    }
+
+    return context
+}
