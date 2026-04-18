0fea7fe8aa10a24e80ff0c01d7f3603c6a9a8359
aleksander <aprokhorenkodev@gmail.com>
2026-04-18 13:18:32 +0200

    feat: add new method to set the context

--- front/context/AuthContext.tsx
+++ front/context/AuthContext.tsx
@@ -3,7 +3,15 @@
 import React, { createContext, useContext, useState } from "react"
 import { ProfileSummary } from "../types/user"
 
-const AuthContext = createContext<ProfileSummary | null>(null)
+// 1. Nuevo tipo
+interface AuthContextType {
+    profile: ProfileSummary | null
+    logOut: () => void
+    login: (profile: ProfileSummary) => void
+}
+
+// 2. Actualizar el createContext
+const AuthContext = createContext<AuthContextType | null>(null)
 
 interface Props {
     children: React.ReactNode,
@@ -12,17 +20,27 @@ interface Props {
 
 export const AuthProvider = ({ children, initialData }: Props) => {
 
-    const [profileState] = useState<ProfileSummary | null>(initialData)
+    const [profileState, setProfileState] = useState<ProfileSummary | null>(initialData)
+
+    const logOut = () => {
+        setProfileState(null)
+    }
+
+    const login = (profile: ProfileSummary) => {
+        setProfileState(profile)
+    }
 
     return (
-        <AuthContext.Provider value={profileState}>
+        <AuthContext.Provider value={{ profile: profileState, logOut, login }}>
             {children}
         </AuthContext.Provider>
     )
 }
 
-export function useAuth(): ProfileSummary | null {
+// 3. Actualizar el tipo de retorno
+export function useAuth(): AuthContextType {
     const context = useContext(AuthContext)
+    if (!context) throw new Error("useAuth must be used within an AuthProvider")
 
     return context
 }
