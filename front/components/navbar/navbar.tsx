4fd75b07faa866a9297cb3f4ac390b39c928f882
aleksander <aprokhorenkodev@gmail.com>
2026-04-18 13:19:13 +0200

    refactor: separate the logic and call the new component

--- front/components/navbar/navbar.tsx
+++ front/components/navbar/navbar.tsx
@@ -2,22 +2,13 @@
 
 
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuList, NavigationMenuTrigger } from "../ui/navigation-menu"
-import { useAuth } from "../../context/AuthContext"
 import { Konkhmer } from "../../app/fonts/fonts"
-import { getNavItems } from "./NavItemsDropDown"
-import { Button } from "../forms/parts/button"
-import { LogOut, User } from "lucide-react"
-import { logOut } from "../../actions/auth"
+import { User } from "lucide-react"
 import { NavBarLink } from "./Link"
-import { ListItem } from "./Item"
-import { useActionState } from "react"
+import { UserMenu } from "./UserMenu"
 
 
 export const CustomNavBar = () => {
-    const profile = useAuth();
-    const components = getNavItems(profile);
-
-
     return (
         <header className="flex items-center justify-around p-2 border-b border-secondary">
             <div className={`flex gap-2 tracking-widest ${Konkhmer.className}`}>
@@ -42,30 +33,7 @@ export const CustomNavBar = () => {
                             <User />
                         </NavigationMenuTrigger>
                         <NavigationMenuContent >
-                            <ul className="flex flex-col gap-1 w-2xs">
-                                {components.map((item) => {
-
-                                    if (item.title === "Cerrar sesión") {
-                                        return <Button
-                                            key={item.title}
-                                            type="button"
-                                            variant="danger"
-                                            onClick={}
-                                            disabled={}
-                                        >
-                                            <LogOut size={16} /> {item.title}
-                                        </Button>
-                                    }
-
-                                    return (
-                                        <ListItem
-                                            key={item.title}
-                                            title={item.title}
-                                            href={item.href}
-                                        />
-                                    );
-                                })}
-                            </ul>
+                            <UserMenu />
                         </NavigationMenuContent>
                     </NavigationMenuItem>
                 </NavigationMenuList>
