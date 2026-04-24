843967a4e61e38fe57db73c4682ae2f588589228
Aleksander <aprokhorenkodev@gmail.com>
2026-04-24 15:00:11 +0200

    feat: decorate the background of the dashboard

--- front/components/dashboard/sidebarWrapper.tsx
+++ front/components/dashboard/sidebarWrapper.tsx
@@ -2,16 +2,22 @@
 
 import React, { useState } from "react"
 import { SideBar } from "./sidebar";
+import { BackgroundDecor } from "../elements/BackgroundDecor";
 
 export const SidebarWrapper = ({ children }: { children: React.ReactNode }) => {
 
     const [isOpen, setOpen] = useState<boolean>(true);
 
     return (
-        <main className={`min-h-svh grid ${isOpen ? "grid-cols-[300px_1fr]" : "grid-cols-[50px_1fr]"} transition-all duration-300`}>
+        <main className={` min-h-svh grid ${isOpen ? "grid-cols-[300px_1fr]" : "grid-cols-[50px_1fr]"} transition-all duration-300`}>
             <SideBar isOpen={isOpen} toggleOpen={() => setOpen(!isOpen)} />
-            <article className="border border-primary p-2">
+            <article className="p-2 flex items-center justify-center relative overflow-hidden">
                 {children}
+                <BackgroundDecor position="top-6 right-8">{"{"}</BackgroundDecor>
+                <BackgroundDecor position="top-1/3 left-6">{"<"}</BackgroundDecor>
+                <BackgroundDecor position="bottom-10 right-1/4">{">"}</BackgroundDecor>
+                <BackgroundDecor position="bottom-6 left-10">{";"}</BackgroundDecor>
+                <BackgroundDecor position="top-1/2 right-6">{"}"}</BackgroundDecor>
             </article>
         </main>
     )
