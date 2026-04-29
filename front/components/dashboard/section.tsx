3608d79f04b648438c31803c3c3e93a31a11711f
Aleksander <aprokhorenkodev@gmail.com>
2026-04-29 17:00:34 +0200

    feat: change the colors of the section

--- front/components/dashboard/section.tsx
+++ front/components/dashboard/section.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 
-export const Section = ({ children }: { children: React.ReactNode }) => {
+export const Section = ({ children, className }: { children: React.ReactNode, className?: string }) => {
     return (
-        <section className="size-9/10 rounded-sm bg-muted-foreground/80 overflow-y-auto fixex z-2">
+        <section className={`size-9/10 rounded-sm bg-border/80 overflow-y-auto fixex z-2 ${className}`}>
             {children}
         </section>
     )
