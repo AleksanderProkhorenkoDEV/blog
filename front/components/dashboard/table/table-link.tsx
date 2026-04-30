e70dcceb2631537dc358d302f6d4cb3c489d25be
Aleksander <aprokhorenkodev@gmail.com>
2026-04-30 17:06:26 +0200

    style: center the item

--- front/components/dashboard/table/table-link.tsx
+++ front/components/dashboard/table/table-link.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export const TableLink = ({ children, href }: { children: React.ReactNode, href: string }) => {
 
     return (
-        <Link href={href} className="ease-in-out flex gap-0.5 group-hover:text-primary transition-text duration-200">
+        <Link href={href} className="ease-in-out flex justify-center items-cen gap-0.5 group-hover:text-primary transition-text duration-200">
             {children}
             <MoveUpRight width={16} className="text-primary opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
         </Link>
