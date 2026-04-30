8dad7eef5988de451a5b96f5484aa2ce9c7808a1
Aleksander <aprokhorenkodev@gmail.com>
2026-04-30 17:04:40 +0200

    feat: create function to get post

---
+++ front/lib/data/posts.ts
@@ -0,0 +1,25 @@
+import prisma from "../prisma/prisma"
+
+export const getPost = async (page: number = 1, limit: number = 10) => {
+    const [posts, total] = await prisma.$transaction([
+        prisma.post.findMany({
+            select: {
+                title: true,
+                slug: true,
+                published: true,
+                author: {
+                    select: {
+                        name: true
+                    }
+                },
+                createdAt: true
+            },
+            skip: page * limit,
+            take: limit,
+            orderBy: { createdAt: "desc" },
+        }),
+        prisma.post.count()
+    ]);
+
+    return { posts, total, pages: Math.ceil(total / limit) }
+}
