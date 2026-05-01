be6ec39e1a08288a0af78566e2901e56eb1a136d
aleksander <aprokhorenkodev@gmail.com>
2026-05-01 12:41:41 +0200

    feat: create component to empty tables

---
+++ front/components/elements/table-empty.tsx
@@ -0,0 +1,24 @@
+import { jetBrain } from "../../app/fonts/fonts"
+import { Td } from "../dashboard/table/td"
+import { Tr } from "../dashboard/table/tr"
+
+export const TableEmpty = () => {
+    return (
+        <Tr>
+            <Td colSpan={6} className="py-12 text-center">
+                <div className="flex flex-col items-center gap-2 text-secondary">
+                    <span className={`text-4xl ${jetBrain.className}`}>404</span>
+                    <span className={`text-sm ${jetBrain.className}`}>
+                        <span className="text-primary">null</span>
+                        {" !== "}
+                        <span className="text-primary">undefined</span>
+                        {", pero aquí no hay posts"}
+                    </span>
+                    <span className={`text-xs opacity-50 ${jetBrain.className}`}>
+                        {"//TODO: crear algún post"}
+                    </span>
+                </div>
+            </Td>
+        </Tr>
+    )
+}
