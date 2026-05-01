7c68250cb4d9e4f2853f8bbdd36b92791fa59afe
aleksander <aprokhorenkodev@gmail.com>
2026-05-01 13:24:22 +0200

    feat: add classname prop

--- front/components/dashboard/table/th.tsx
+++ front/components/dashboard/table/th.tsx
@@ -1,5 +1,5 @@
-export const Th = ({ children }: { children: React.ReactNode }) => {
+export const Th = ({ children, className }: { children: React.ReactNode, className?: string }) => {
     return (
-        <th className="text-center align-middle font-medium p-4">{children}</th>
+        <th className={`text-center align-middle font-medium p-4 ${className}`}>{children}</th>
     )
 }
