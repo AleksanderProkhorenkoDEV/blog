dc15c0264c9b5d132c169b2e5bfc4d8543ac0fcd
aleksander <aprokhorenkodev@gmail.com>
2026-05-01 12:42:41 +0200

    feat: create pagination for tables

---
+++ front/components/dashboard/table/pagination/table-pagination.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import { useRouter, useSearchParams } from "next/navigation"
+import { jetBrain } from "../../../../app/fonts/fonts"
+import { Button } from "../../../forms/parts/button"
+import { MoveLeft, MoveRight } from "lucide-react"
+import { Td } from "../td"
+import { Tr } from "../tr"
+
+export const TablePagination = ({ totalPages }: { totalPages: number }) => {
+
+    const searchParams = useSearchParams()
+    const router = useRouter()
+
+    const handlePreviusPage = () => {
+        const currentPage = Number(searchParams.get('page') ?? 1);
+        if (currentPage > 1) {
+            handleUpdateUrl(currentPage - 1)
+        }
+    }
+
+    const handleNextPage = () => {
+        const currentPage = Number(searchParams.get('page') ?? 1);
+        if (currentPage <= totalPages) {
+            handleUpdateUrl(currentPage + 1)
+        }
+    }
+
+    const handleUpdateUrl = (pageNumber: number) => {
+        const params = new URLSearchParams(searchParams)
+        params.set('page', String(pageNumber))
+        router.replace(`?${params.toString()}`)
+    }
+
+    const currentPage = Number(searchParams.get('page') ?? 1);
+
+    return (
+
+        <Tr>
+            <Td colSpan={5}>{null}</Td>
+            <Td colSpan={1} className="p-2 border-b-0">
+                <div className="flex items-center gap-2 w-full justify-end">
+                    <Button
+                        type="button"
+                        variant="icons"
+                        onClick={handlePreviusPage}
+                        disabled={currentPage <= 1}
+                    >
+                        <MoveLeft width={18} />
+                    </Button>
+                    <span className={`text-xs ${jetBrain.className}`}>
+                        {currentPage} / {totalPages}
+                    </span>
+                    <Button
+                        type="button"
+                        variant="icons"
+                        onClick={handleNextPage}
+                        disabled={currentPage >= totalPages}
+                    >
+                        <MoveRight width={18} />
+                    </Button>
+                </div>
+            </Td>
+        </Tr>
+    )
+}
