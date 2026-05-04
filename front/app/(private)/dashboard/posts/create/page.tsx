0e305e695d4ff7b46ec9c2c6b80099bdb7205d7b
Aleksander <aprokhorenkodev@gmail.com>
2026-05-04 17:04:03 +0200

    feat: start to customizate the page

---
+++ front/app/(private)/dashboard/posts/create/page.tsx
@@ -0,0 +1,11 @@
+import { Section } from "../../../../../components/dashboard/section";
+import { TipTap } from "../../../../../components/editor/tip-tap";
+
+
+export default function CreatePost() {
+    return (
+        <Section>
+            <TipTap placeholder="Crea un post" />
+        </Section>
+    )
+}
