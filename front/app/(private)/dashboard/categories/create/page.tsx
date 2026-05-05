2ef2d3a9bb54a63ffb77017ab64fb757cabd0e60
Aleksander <aprokhorenkodev@gmail.com>
2026-05-05 16:21:44 +0200

    feat: create page to create categorie

---
+++ front/app/(private)/dashboard/categories/create/page.tsx
@@ -0,0 +1,9 @@
+import { Section } from "@/components/dashboard/section";
+
+export default function CreateCategorie() {
+    return (
+        <Section>
+            <h1>Create categorie</h1>
+        </Section>
+    )
+}
