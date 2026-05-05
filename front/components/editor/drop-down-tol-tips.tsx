87cfe33531f4f8b595f3f3896ca723d1c43d71d2
Aleksander <aprokhorenkodev@gmail.com>
2026-05-05 14:02:03 +0200

    refactor: change the colors

--- front/components/editor/drop-down-tol-tips.tsx
+++ front/components/editor/drop-down-tol-tips.tsx
@@ -18,20 +18,22 @@ export const DropDownTolTips = ({ item }: Props) => {
     const Icon = item.icon;
     return (
         <div className="relative group/dropdown cursor-pointer">
-            <Icon size={16} />
+            <div className="px-0.5 text-popover hover:bg-muted rounded">
+                <Icon size={16} />
+            </div>
 
             <div className="
                 absolute top-full left-0
                 hidden group-hover/dropdown:block
                 pt-1 z-20
             ">
-                <div className="bg-popover text-popover-foreground border border-border shadow-md p-2 rounded-sm">
+                <div className="bg-popover border border-border shadow-md p-2 rounded-sm flex flex-col gap-0.5">
                     {item.items.map((subItem, i) => (
                         <TolTip
                             key={`${subItem.command}-${i}`}
                             message={subItem.message}
                             command={subItem.command}
-                            customStyle={subItem.customStyle}
+                            customStyle={`${subItem.customStyle} !text-foreground !hover:bg-foreground/10`}
                         />
                     ))}
                 </div>
