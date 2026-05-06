a24e506a92e8af69f39eadb8581461d7a75a0831
Aleksander <aprokhorenkodev@gmail.com>
2026-05-06 14:08:53 +0200

    fix: count and add cache

--- front/lib/data/categories.ts
+++ front/lib/data/categories.ts
@@ -1,16 +1,20 @@
+import { cacheLife, cacheTag } from "next/cache";
 import prisma from "../prisma/prisma";
 
 export const getCategories = async (page: number = 1, limit: number = 10) => {
+    'use cache'
+    cacheTag("category")
+    cacheLife("weeks")
     const [categories, total] = await prisma.$transaction([
         prisma.category.findMany({
             select: {
                 id: true,
-                name:true,
+                name: true,
             },
             skip: (page - 1) * limit,
             take: limit,
         }),
-        prisma.post.count()
+        prisma.category.count()
     ]);
 
     return { categories, total, pages: Math.ceil(total / limit) }
