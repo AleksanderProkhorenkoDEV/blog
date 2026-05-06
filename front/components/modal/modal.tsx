22f6ca3f8039d80cad377ba111fe71d21ae65075
Aleksander <aprokhorenkodev@gmail.com>
2026-05-06 14:09:19 +0200

    feat: customize the modal and use use Router

--- front/components/modal/modal.tsx
+++ front/components/modal/modal.tsx
@@ -1,9 +1,43 @@
-import React from "react"
+'use client'
 
-export const Modal = ({ children }: { children: React.ReactNode }) => {
+import { Button } from "@/components/forms/parts/button";
+import { Konkhmer } from "@/app/fonts/fonts";
+import { X } from "lucide-react";
+import React from "react"
+import { useRouter } from "next/navigation";
+
+interface Props {
+    children: React.ReactNode;
+    title: string;
+}
+
+export const Modal = ({ children, title }: Props) => {
+
+    const router = useRouter()
+
     return (
-        <article className="fixed z-50 rounded-sm bg-foreground text-popover p-2">
-            {children}
-        </article>
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+            <div className="absolute inset-0 bg-popover/30 backdrop-blur-xs"></div>
+            <article
+                className="relative z-10 bg-foreground text-popover 
+                            p-3 rounded-xl shadow-lg min-w-2xl flex flex-col gap-4
+                            animate-in fade-in zoom-in-95 duration-200 ease-out
+                        "
+                role="dialog"
+                aria-modal="true"
+            >
+                <header className="flex justify-between">
+                    <p className={`${Konkhmer.className} tracking-wide text-xl`}>{title}</p>
+                    <Button
+                        type="button"
+                        variant="icons"
+                        onClick={() => router.back()}
+                    >
+                        <X />
+                    </Button>
+                </header>
+                {children}
+            </article>
+        </div>
     )
 }
