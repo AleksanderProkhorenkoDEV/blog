81f897487c8d95f33cd0c1e6126eb995e85f0156
Aleksander <aprokhorenkodev@gmail.com>
2026-05-11 13:57:56 +0200

    refactor: dahsboard layout persist maybe i will use it for get request

--- front/app/(private)/dashboard/layout.tsx
+++ front/app/(private)/dashboard/layout.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function DashboardLayout({ children, modal }: { children: React.ReactNode, modal: React.ReactNode }) {
+// DONT REMOVE, USE IT FOR INTERCEP ROUTES AND DISPLAY MODAL WITH PARALLEL ROUTES
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
             {children}
-            {modal}
         </>
     )
 }
