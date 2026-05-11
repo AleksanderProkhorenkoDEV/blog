4ac997e8e21a48f8185bd06d9e5afbc6ab633948
Aleksander <aprokhorenkodev@gmail.com>
2026-05-11 17:02:53 +0200

    feat: create new method to get data

--- front/lib/data/categories.ts
+++ front/lib/data/categories.ts
@@ -19,3 +19,21 @@ export const getCategories = async (page: number = 1, limit: number = 10) => {
 
     return { categories, total, pages: Math.ceil(total / limit) }
 }
+
+export const getCategory = async (id: string) => {
+    'use cache'
+    cacheTag(`category-${id}`)
+    cacheLife("days")
+
+    const idParse = Number(id)
+
+    if (!Number.isInteger(idParse) || idParse <= 0) {
+        throw new Error("ID inválido")
+    }
+
+    const category = await prisma.category.findUnique({
+        where: { id: idParse }
+    })
+
+    return category;
+}
