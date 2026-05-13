f384de46f76ff22d4e98eab6d45dc1f5a8b76a2f
Aleksander <aprokhorenkodev@gmail.com>
2026-05-13 16:57:12 +0200

    feat: call the server action and pass the data

--- front/app/(private)/dashboard/posts/create/page.tsx
+++ front/app/(private)/dashboard/posts/create/page.tsx
@@ -1,12 +1,18 @@
 import { Section } from "@/components/dashboard/layout/section";
 import { PostForm } from "@/components/forms/post-form";
+import { getCategories } from "@/lib/data/categories";
+import { toSelectOptions } from "@/lib/utils/select";
 
 
 
-export default function CreatePost() {
+export default async function CreatePost() {
+
+    const categories = await getCategories()
+    const categoriesOption = toSelectOptions(categories.categories, "id", "name");
+
     return (
-        <Section>
-           <PostForm />
+        <Section className="p-4">
+            <PostForm categories={categoriesOption} />
         </Section>
     )
 }
