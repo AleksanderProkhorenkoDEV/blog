a12c237db052cffd685e225a630d1743c6a2d0b5
Aleksander <aprokhorenkodev@gmail.com>
2026-05-13 16:55:52 +0200

    feat: create a custom select input

---
+++ front/components/forms/parts/select.tsx
@@ -0,0 +1,44 @@
+interface Props {
+    name: string,
+    multiple?: boolean,
+    options: OptionSelect[],
+    onChange?: (value: string) => void
+    variant?: "light" | "dark",
+    className?: string
+
+}
+
+export type OptionSelect = {
+    value: string | number;
+    label: string
+}
+
+const variants = {
+    light: "bg-foreground text-popover",
+    dark: "border border-popover shadow-lg",
+}
+
+export const CustomSelect = ({ name, multiple = false, options, onChange, variant = "light", className }: Props) => {
+    return (
+        <select
+            name={name}
+            multiple={multiple}
+            onChange={(e) => onChange?.(e.target.value)}
+            className={`${variants[variant]} rounded-sm p-2 min-w-fit ${multiple ? "h-16" : ""} ${className}`}
+        >
+            {
+                options.map((item, index) => {
+                    return (
+                        <option
+                            key={`${item.label}-${index}`}
+                            value={item.value}
+                            className={`hover:bg-primary px-0.5 rounded-sm transition duration-200 ease-in-out cursor-pointer mb-1`}
+                        >
+                            {item.label}
+                        </option>
+                    )
+                })
+            }
+        </select>
+    )
+}
