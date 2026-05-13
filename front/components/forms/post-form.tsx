de935d84a87c4124452c286355409747fb0a6af1
Aleksander <aprokhorenkodev@gmail.com>
2026-05-13 16:56:13 +0200

    refactor: obtain the categorys in the main page

--- front/components/forms/post-form.tsx
+++ front/components/forms/post-form.tsx
@@ -3,19 +3,41 @@
 import { CustomInput } from "@/components/forms/parts/input"
 import { Button } from "@/components/forms/parts/button"
 import { TipTap } from "@/components/editor/tip-tap"
+import { CustomSelect, OptionSelect } from "./parts/select"
+import { CategoryBadget } from "../dashboard/badgets/category-badget"
 
-export const PostForm = () => {
+interface Props {
+    categories: OptionSelect[]
+}
+
+export const PostForm = ({ categories }: Props) => {
     return (
         <form className="border border-primary w-5xl m-auto p-2 flex flex-col gap-1">
             <label htmlFor="name">Titulo del post</label>
             <CustomInput type="text" name="title" />
             <label htmlFor="name">Slug</label>
             <CustomInput type="text" name="slug" />
+            <div className="flex flex-col">
+                <label htmlFor="categories">Categorías</label>
+                <div className="flex gap-4 border p-2">
+                    <CustomSelect
+                        name="categories"
+                        multiple
+                        options={categories}
+                        className="w-md"
+                    />
+                    <div>
+                        <CategoryBadget name="Jetpack Compose"/>
+                    </div>
+                </div>
+            </div>
             <label htmlFor="name">Contenido</label>
             <TipTap placeholder="Crea un post" />
-            <Button type="submit" variant="primary">
-                Crear articulo
-            </Button>
+            <footer>
+                <Button type="submit" variant="primary" >
+                    Crear articulo
+                </Button>
+            </footer>
         </form>
     )
 }
