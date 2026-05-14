12f96f6cac0cc569b26a6fdac8deef4bc9b84a41
Aleksander <aprokhorenkodev@gmail.com>
2026-05-14 16:04:33 +0200

    feat: allow domain of supabase to next/image

--- front/next.config.ts
+++ front/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     cacheComponents: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cjhfthijonzdcdolhinp.supabase.co',
+       
+            }
+        ]
+    }
 };
 
 export default nextConfig;
