51d7185b4700d85affa183712e7061e3f95fdac6
Aleksander <aprokhorenkodev@gmail.com>
2026-05-14 14:01:50 +0200

    style: add max height to the badget

--- front/components/dashboard/badgets/category-badget.tsx
+++ front/components/dashboard/badgets/category-badget.tsx
@@ -3,7 +3,7 @@ import { Tag } from "lucide-react"
 export const CategoryBadget = ({ name }: { name: string }) => {
 
     return (
-        <span className="flex gap-2 items-center border border-border text-foreground/60 bg-secondary/20 hover:border-primary p-2 rounded-lg hover:bg-primary/20 hover:text-primary duration-200 transition ease-in-out">
+        <span className="flex gap-2 max-h-8 items-center border border-border text-foreground/60 bg-secondary/20 hover:border-primary p-2 rounded-lg hover:bg-primary/20 hover:text-primary duration-200 transition ease-in-out">
             <Tag/>
             {name}
         </span>
