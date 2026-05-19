8093e209e72439e233ed02080a483c5225e13c6b
Aleksander <aprokhorenkodev@gmail.com>
2026-05-19 17:00:23 +0200

    feat: start to use custom hook dont work

--- front/components/forms/post-form.tsx
+++ front/components/forms/post-form.tsx
@@ -4,19 +4,22 @@ import { CategoryBadget } from "../dashboard/badgets/category-badget"
 import { CustomInput } from "@/components/forms/parts/input"
 import { CustomSelect, OptionSelect } from "./parts/select"
 import { Button } from "@/components/forms/parts/button"
-import { TipTap } from "@/components/editor/tip-tap"
 import { CustomInputFiles } from "./parts/input-file"
+import { TipTap } from "@/components/editor/tip-tap"
+import { uploadImage } from "@/lib/actions/storage"
 import { ImageUp } from "lucide-react"
-import { useState } from "react"
+import { SubmitEventHandler, useState } from "react"
 import { toast } from "sonner"
 import Image from "next/image"
-import { uploadImage } from "@/lib/actions/storage"
+import { usePostCreate } from "@/hooks/usePostCreate"
 
 interface Props {
     categories: OptionSelect[]
+    authorId: string | undefined;
 }
 
-export const PostForm = ({ categories }: Props) => {
+export const PostForm = ({ categories, authorId }: Props) => {
+
 
     const [selectedCategory, setSelectedCategory] = useState<OptionSelect[]>([])
     const [thumbnail, setThumbnail] = useState<string>("/working-code.webp")
@@ -27,7 +30,6 @@ export const PostForm = ({ categories }: Props) => {
     }
 
     const handleImageUpload = async (value: File) => {
-        console.log(value);
         const maxSize = 2 * 1024 * 1024
         if (value.size > maxSize) {
             toast.error("La imagen pesa mucho, no puedes subirla")
@@ -42,62 +44,85 @@ export const PostForm = ({ categories }: Props) => {
         setThumbnail(result.url!)
     }
 
+    const { loading, handleCreatePost } = usePostCreate(authorId)
+
     return (
         <form
-            className="border border-primary w-5xl m-auto p-2 flex flex-col gap-1"
+            className="w-5xl m-auto p-4 flex flex-col gap-1 bg-card text-card rounded-md"
+            onSubmit={handleCreatePost}
         >
-            <div className="flex gap-2">
-                <div className="flex flex-col flex-1">
-                    <label htmlFor="title">Titulo del post</label>
-                    <CustomInput type="text" name="title" />
-                    <label htmlFor="slug">Slug</label>
-                    <CustomInput type="text" name="slug" />
+            {/* Metadatos y Portada */}
+            <div className="flex gap-4 justify-center">
+                <div className="flex-1">
+                    <h1 className="uppercase text-md tracking-wider mb-2 text-foreground/60">Metadatos</h1>
+                    <label htmlFor="title" className="flex flex-col gap-2 text-foreground">
+                        Titulo del post
+                        <CustomInput type="text" name="title" variant="light" />
+                    </label>
+                    <label htmlFor="slug" className="flex flex-col gap-2 text-foreground">
+                        Slug
+                        <CustomInput type="text" name="slug" variant="light" />
+                    </label>
                 </div>
-                <div className="flex-1 flex flex-col items-end gap-2">
-                    <Image src={thumbnail} alt="thumbnail" width={400} height={400} />
-                    <CustomInputFiles
-                        name="thumbnail"
-                        accept="image/jpeg, image/webp"
-                        onChange={handleImageUpload}
-                        className=""
-                    >
-                        <ImageUp />
-                        Añade una imagen de portada
-                    </CustomInputFiles>
-                </div>
-            </div>
-
-            <div className="flex flex-col">
-                <label htmlFor="categories" className="font-semibold mb-1">Categorías</label>
-                <div className="flex gap-4 ">
-                    <CustomSelect
-                        name="categories"
-                        multiple
-                        options={categories}
-                        className="flex-1"
-                        onChange={handleChangeSelect}
-                    />
-                    <div className="flex flex-1 flex-wrap gap-2">
-                        {
-                            selectedCategory.length != 0 ?
-                                selectedCategory.map((item) => {
-                                    return (
-                                        <CategoryBadget key={item.value} name={item.label} />
-                                    )
-                                })
-                                : <p>No has seleccionado ninguna categoría</p>
-                        }
+                <div className="flex-1">
+                    <h1 className="uppercase text-md tracking-wider text-foreground/60 mb-2">Portada</h1>
+                    <div className="relative aspect-video rounded-md overflow-hidden">
+                        <Image
+                            src={thumbnail}
+                            alt="thumbnail"
+                            fill
+                            className="object-cover"
+                        />
+                        <div className="absolute inset-0 flex flex-col items-center justify-center gap-2 bg-background/50 opacity-0 hover:opacity-100 transition-opacity cursor-pointer">
+                            <ImageUp className="text-foreground" />
+                            <CustomInputFiles
+                                name="thumbnail"
+                                onChange={handleImageUpload}
+                                className="text-foreground"
+                            >
+                                Actualizar imagen
+                            </CustomInputFiles>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="flex gap-2 items-center w-fit mt-1">
-                <label htmlFor="published">Publicar articulo</label>
-                <CustomInput name="published" type="checkbox" />
+            <hr className="border-t border-secondary my-4" />
+            {/* Categorías y estado de publicación */}
+            <div className="flex flex-wrap gap-4">
+                <label htmlFor="categories" className="uppercase text-md tracking-wider text-foreground/60 mb-2 flex-1">
+                    Categorías
+                    <div className="flex flex-col gap-4 mt-2">
+                        <CustomSelect
+                            name="categories"
+                            multiple
+                            variant="light"
+                            options={categories}
+                            className="flex-1"
+                            onChange={handleChangeSelect}
+                        />
+                        <div className="flex flex-1 flex-wrap gap-2">
+                            {
+                                selectedCategory.length != 0 ?
+                                    selectedCategory.map((item) => {
+                                        return (
+                                            <CategoryBadget key={item.value} name={item.label} variant="dark" />
+                                        )
+                                    })
+                                    : <p>No has seleccionado ninguna categoría</p>
+                            }
+                        </div>
+                    </div>
+                </label>
+                <div className="flex-1 flex items-center  h-fit gap-1">
+                    <CustomInput name="published" type="checkbox" />
+                    <label htmlFor="published" className="text-foreground">Borrador</label>
+                </div>
             </div>
-            <label htmlFor="name">Contenido</label>
+            <hr className="border-t border-secondary mb-2" />
+            <label htmlFor="name" className="uppercase text-foreground/60 tracking-wider">Contenido</label>
             <TipTap placeholder="Crea un post" />
-            <footer>
-                <Button type="submit" variant="primary" >
+            <footer className="w-full flex justify-end">
+                <Button type="submit" variant="primary" disabled={loading} >
                     Crear articulo
                 </Button>
             </footer>
