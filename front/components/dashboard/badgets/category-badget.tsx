8bca3f202e4e8778600eb46b94644721bfbafb0a
Aleksander <aprokhorenkodev@gmail.com>
2026-05-19 17:00:36 +0200

    feat: add variant styles

--- front/components/dashboard/badgets/category-badget.tsx
+++ front/components/dashboard/badgets/category-badget.tsx
@@ -1,10 +1,27 @@
+import { cn } from "@/lib/utils"
 import { Tag } from "lucide-react"
 
-export const CategoryBadget = ({ name }: { name: string }) => {
+type BadgetVariant = "light" | "dark"
+
+const variants: Record<BadgetVariant, string> = {
+    light: "border border-border  bg-secondary/20 text-foreground/60 hover:border-primary hover:bg-primary/20 hover:text-primary",
+    dark: "border border-border/40  bg-background/10  hover:border-primary/70 hover:bg-primary/20 hover:text-primary",
+}
+
+interface Props {
+    name: string
+    variant?: BadgetVariant
+}
+
+
+export const CategoryBadget = ({ name, variant = "light" }: Props) => {
 
     return (
-        <span className="flex gap-2 max-h-8 items-center border border-border text-foreground/60 bg-secondary/20 hover:border-primary p-2 rounded-lg hover:bg-primary/20 hover:text-primary duration-200 transition ease-in-out">
-            <Tag/>
+        <span className={cn(
+            "flex gap-2 max-h-8 items-center p-2 rounded-lg duration-200 transition ease-in-out",
+            variants[variant]
+        )}>
+            <Tag size={16} />
             {name}
         </span>
     )
