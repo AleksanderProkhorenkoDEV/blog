2a2a625c3517f0baa3aa4cde84aa9494879291df
Aleksander <aprokhorenkodev@gmail.com>
2026-05-19 17:00:44 +0200

    feat: pass the user id to the post form

--- front/app/(private)/dashboard/posts/create/page.tsx
+++ front/app/(private)/dashboard/posts/create/page.tsx
@@ -2,6 +2,7 @@ import { Section } from "@/components/dashboard/layout/section";
 import { getAllCategories } from "@/lib/data/categories";
 import { PostForm } from "@/components/forms/post-form";
 import { toSelectOptions } from "@/lib/utils/select";
+import { createClient } from "@/lib/supabase/server";
 
 
 
@@ -9,10 +10,15 @@ export default async function CreatePost() {
 
     const categories = await getAllCategories()
     const categoriesOption = toSelectOptions(categories, "id", "name");
+    const supabase = await createClient()
+    const { data: { user } } = await supabase.auth.getUser()
 
     return (
         <Section className="p-4">
-            <PostForm categories={categoriesOption} />
+            <PostForm
+                categories={categoriesOption}
+                authorId={user?.id}
+            />
         </Section>
     )
 }
