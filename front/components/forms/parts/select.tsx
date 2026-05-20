907c83d218b8b2eea9ea2ea730aa8289260ef5b9
Aleksander <aprokhorenkodev@gmail.com>
2026-05-20 15:40:12 +0200

    feat: add error message in the select

--- front/components/forms/parts/select.tsx
+++ front/components/forms/parts/select.tsx
@@ -4,7 +4,8 @@ interface Props {
     options: OptionSelect[],
     onChange?: (value: string[]) => void
     variant?: "light" | "dark",
-    className?: string
+    className?: string,
+    error: string | undefined
 
 }
 
@@ -18,35 +19,48 @@ const variants = {
     dark: "border border-popover shadow-lg",
 }
 
-export const CustomSelect = ({ name, multiple = false, options, onChange, variant = "light", className }: Props) => {
+export const CustomSelect = ({ name, multiple = false, options, onChange, variant = "light", className, error }: Props) => {
     return (
-        <select
-            id={name}
-            name={name}
-            multiple={multiple}
-            onChange={(e) => {
-                if (multiple) {
-                    const selected = Array.from(e.target.selectedOptions).map(opt => opt.value)
-                    onChange?.(selected)
-                } else {
-                    onChange?.([e.target.value])
+
+        <>
+            <select
+                id={name}
+                name={name}
+                multiple={multiple}
+                onChange={(e) => {
+                    if (multiple) {
+                        const selected = Array.from(e.target.selectedOptions).map(opt => opt.value)
+                        onChange?.(selected)
+                    } else {
+                        onChange?.([e.target.value])
+                    }
+                }}
+                className={`${variants[variant]} rounded-sm p-2 ${multiple ? "h-32" : ""} ${className}`}
+            >
+                {
+                    options.map((item, index) => {
+                        return (
+                            <option
+                                key={`${item.label}-${index}`}
+                                value={item.value}
+                                className={`hover:bg-primary px-0.5 rounded-sm transition duration-200 ease-in-out cursor-pointer mb-1`}
+                            >
+                                {item.label}
+                            </option>
+                        )
+                    })
                 }
-            }}
-            className={`${variants[variant]} rounded-sm p-2 ${multiple ? "h-32" : ""} ${className}`}
-        >
+            </select>
             {
-                options.map((item, index) => {
-                    return (
-                        <option
-                            key={`${item.label}-${index}`}
-                            value={item.value}
-                            className={`hover:bg-primary px-0.5 rounded-sm transition duration-200 ease-in-out cursor-pointer mb-1`}
-                        >
-                            {item.label}
-                        </option>
-                    )
-                })
+                error ? <p className={`min-h-5 text-sm capitalize ${error
+                    ? "text-destructive before:content-['*'] before:mr-0.5"
+                    : ""
+                    }`}
+                >
+                    {error ?? ""}
+                </p> : null
             }
-        </select>
+        </>
+
     )
 }
