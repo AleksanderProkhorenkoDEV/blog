f78abcc67b4682bb0ca3d696f67030f7b3440875
Aleksander <aprokhorenkodev@gmail.com>
2026-05-20 15:39:03 +0200

    fix: error with the validations

--- front/schemas/post.ts
+++ front/schemas/post.ts
@@ -9,7 +9,7 @@ export const postSchema = z.object({
         .regex(/^[a-z]+(-[a-z]+)*$/, { error: "El slug solo puede contener letras minúsculas y guiones" }),
     authorId: z.uuid(),
     thumbnail: z.url({ error: "El thumbnail debe ser una URL válida" }),
-    categories: z.array(z.uuid()).min(1, { error: "Selecciona al menos una categoría" }),
+    categories: z.array(z.string()).min(1, { error: "Selecciona al menos una categoría" }),
     published: z.boolean(),
     content: z.string().min(1),
 })
