31d220cd455d2b8cd6ecc2a482f6f9a3118f3c85
Aleksander <aprokhorenkodev@gmail.com>
2026-05-25 12:19:51 +0200

    feat: create update extema extending the create

--- front/schemas/post.ts
+++ front/schemas/post.ts
@@ -6,10 +6,14 @@ export const postSchema = z.object({
         .regex(/^[a-zA-ZÀ-ÿ0-9\s'-]+$/, { error: "El titulo solo puede contener letras y números" }),
     slug: z.string()
         .min(2, { error: "El slug solo puede ser texto" })
-        .regex(/^[a-z]+(-[a-z]+)*$/, { error: "El slug solo puede contener letras minúsculas y guiones" }),
+        .regex(/^[a-z0-9]+(-[a-z0-9]+)*$/, { error: "El slug solo puede contener letras minúsculas, números y guiones" }),
     authorId: z.uuid(),
     thumbnail: z.url({ error: "El thumbnail debe ser una URL válida" }),
     categories: z.array(z.string()).min(1, { error: "Selecciona al menos una categoría" }),
     published: z.boolean(),
     content: z.string().min(1),
+})
+
+export const updatePostSchema = postSchema.extend({
+    id: z.coerce.number()
 })
