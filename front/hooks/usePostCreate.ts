606666034e01f122d351b8e214a56f97a6f970b7
Aleksander <aprokhorenkodev@gmail.com>
2026-05-25 12:20:32 +0200

    refactor: make the hook posibility to update or create post

--- front/hooks/usePostCreate.ts
+++ front/hooks/usePostCreate.ts
@@ -1,34 +1,32 @@
 'use client'
 
-import { createPost } from "@/lib/actions/post"
+import { createPost, updatePost } from "@/lib/actions/post"
 import { postSchema } from "@/schemas/post"
-import { useRouter } from "next/navigation"
 import { useState } from "react"
 import { toast } from "sonner"
 import z from "zod"
 
-export const usePostCreate = () => {
+export const usePostCreate = (postId?: number) => {
 
 
     const [loading, setLoading] = useState<boolean>(false)
     const [inputErrors, setInputErrors] = useState<Partial<Record<keyof z.infer<typeof postSchema>, string[]>>>({})
 
-    const router = useRouter()
-
     const handleCreatePost = async (formData: FormData) => {
         try {
             setLoading(true)
 
-            const { success, inputErrors } = await createPost(formData)
+            const { success, inputErrors } = postId
+                ? await updatePost(formData)
+                : await createPost(formData)
 
             if (!success) {
                 if (inputErrors) setInputErrors(inputErrors)
                 return
             }
-            toast.success('Post creado')
-            router.back()
+            toast.success(postId ? 'Post actualizado' : 'Post creado')
         } catch (error) {
-            toast.error(`Ha ocurrido un error creando el post: ${error}`)
+            toast.error(`Ha ocurrido un error: ${error}`)
         } finally {
             setLoading(false)
         }
