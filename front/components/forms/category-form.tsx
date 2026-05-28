dea662fe23b7555a406b84a4dbf05d7706286e4e
Aleksander <aprokhorenkodev@gmail.com>
2026-05-28 15:37:09 +0200

    refactor: add redirect in server action

--- front/components/forms/category-form.tsx
+++ front/components/forms/category-form.tsx
@@ -7,7 +7,6 @@ import { Button } from "@/components/forms/parts/button"
 import { FolderPen, FolderPlus } from "lucide-react"
 import { Form } from "@/components/forms/base-form"
 import { useActionState, useEffect } from "react"
-import { useRouter } from "next/navigation"
 import { toast } from "sonner"
 
 interface CategoryFormProps {
@@ -33,18 +32,12 @@ export const CategoryForm = ({ defaultValues }: CategoryFormProps) => {
 
     const [state, formAction, pending] = useActionState(action, initState);
 
-    const router = useRouter();
 
     useEffect(() => {
-        if (state.success) {
-            toast.success(isEditing ? "Categoría actualizada correctamente." : "Categoría creada correctamente.")
-            router.push("/dashboard/categories")
-        }
-
         if (state.formError) {
             toast.error(`Error al ${isEditing ? "actualizar" : "crear"} la categoría: ${state.formError}`)
         }
-    }, [state, router])
+    }, [state, isEditing])
 
     return (
         <Form action={formAction} className="max-sm:min-w-fit">
