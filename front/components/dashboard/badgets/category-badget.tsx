e9377b647926a687d1e2b0915501842404094a82
Aleksander <aprokhorenkodev@gmail.com>
2026-06-02 16:12:00 +0200

    refactor: remove the border

--- front/components/dashboard/badgets/category-badget.tsx
+++ front/components/dashboard/badgets/category-badget.tsx
@@ -4,7 +4,7 @@ import { Tag } from "lucide-react"
 type BadgetVariant = "light" | "dark" | "active"
 
 const variants: Record<BadgetVariant, string> = {
-    light: "border border-border  bg-secondary/20 text-foreground/60 hover:border-primary hover:bg-primary/20 hover:text-primary",
+    light: "bg-secondary/20 text-foreground/60 hover:border-primary hover:bg-primary/20 hover:text-primary",
     dark: "border border-border/40  bg-background/10  hover:border-primary/70 hover:bg-primary/20 hover:text-primary",
     active: "bg-primary/20 text-primary"
 }
@@ -21,7 +21,7 @@ export const CategoryBadget = ({ name, variant = "light", icon = false, classNam
 
     return (
         <span className={cn(
-            `flex gap-2 max-h-8 items-center p-2 rounded-lg duration-200 transition ease-in-out ${className}`,
+            `flex gap-2 max-h-8 items-center p-2 rounded-lg duration-200 transition ease-in-out capitalize px-4 py-2 ${className}`,
             variants[variant]
         )}>
             {icon ? <Tag size={16} /> : null}
