a1a1d4dbabdf5176cba16da7d4b0c4eeae3f49bf
Aleksander <aprokhorenkodev@gmail.com>
2026-06-03 10:02:22 +0200

    refactor: make responsive the main content in all the app

--- front/app/(public)/layout.tsx
+++ front/app/(public)/layout.tsx
@@ -5,7 +5,7 @@ export default function PublicLayout({ children }: { children: React.ReactNode }
     return (
         <>
             <CustomNavBar />
-            <main className="max-w-7xl mx-auto">
+            <main className="max-w-7xl mx-auto max-2xl:w-6xl max-xl:w-4xl max-lg:w-xl max-sm:w-full max-sm:px-8">
                 {children}
             </main>
         </>
