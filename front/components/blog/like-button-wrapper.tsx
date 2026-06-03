b0c72007023da3ce48230892bfca3952eee9704f
Aleksander <aprokhorenkodev@gmail.com>
2026-06-03 16:54:42 +0200

    feat: start to create a wrapper button

---
+++ front/components/blog/like-button-wrapper.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useFormAction } from "@/hooks/useFormAction"
+import { useAuth } from "@/context/AuthContext"
+import { Button } from "../forms/parts/button"
+import { likePost } from "@/lib/actions/post"
+import { Modal } from "../modal/modal"
+import { Heart } from "lucide-react"
+import { useState } from "react"
+
+interface Props {
+    postId: number
+}
+
+export const LikeButtonWrapper = ({ postId }: Props) => {
+
+    const { profile } = useAuth()
+    const [showModal, setShowModal] = useState(false)
+
+    const likePostWithData = likePost.bind(null, postId, profile?.email ?? '')
+    const { dispatch, pending } = useFormAction(likePostWithData)
+
+    if (!profile) {
+        return (
+            <>
+                <Button
+                    type="button"
+                    variant="icons"
+                    className="hover:text-destructive!"
+                    onClick={() => setShowModal(true)}
+                >
+                    <Heart />
+                </Button>
+
+                {showModal && (
+                    <Modal title="Necesitas Iniciar Sesión" onClose={() => setShowModal(false)}>
+                        <p>Para poder darle like a un articulo, necesitas iniciar sesión en la web.</p>
+                    </Modal>
+                )}
+            </>
+        )
+    }
+
+    return (
+        <form action={dispatch}>
+            <Button
+                type="submit"
+                variant="icons"
+                className="hover:text-destructive!"
+                disabled={pending}
+            >
+                <Heart />
+            </Button>
+        </form>
+    )
+}
