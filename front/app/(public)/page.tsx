1005b53d9a343969c93294deff528e302f0ad064
Aleksander <aprokhorenkodev@gmail.com>
2026-09-17 10:59:05 +0200

    feat: add suspense boundary

--- front/app/(public)/page.tsx
+++ front/app/(public)/page.tsx
@@ -1,11 +1,14 @@
 import { BestPost } from "@/components/home/best-post";
 import { HeroSection } from "../../components/home/heroSection";
+import { Suspense } from "react";
 
 export default function HomePage() {
     return (
         <>
             <HeroSection />
-            <BestPost />
+            <Suspense fallback={<p>loading...</p>}>
+                <BestPost />
+            </Suspense>
         </>
     )
 }
