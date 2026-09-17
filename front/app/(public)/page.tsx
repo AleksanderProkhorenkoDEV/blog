d2b78b6c34a9cfc2822ac63371cb6b0c26b99a16
Aleksander <aprokhorenkodev@gmail.com>
2026-09-17 10:42:05 +0200

    feat: add in the page

--- front/app/(public)/page.tsx
+++ front/app/(public)/page.tsx
@@ -1,7 +1,11 @@
+import { BestPost } from "@/components/home/best-post";
 import { HeroSection } from "../../components/home/heroSection";
 
 export default function HomePage() {
     return (
-        <HeroSection />
+        <>
+            <HeroSection />
+            <BestPost />
+        </>
     )
 }
