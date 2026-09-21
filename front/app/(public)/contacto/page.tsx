263f3073b01933b214d30e137e1c60468e9569cf
Aleksander <aprokhorenkodev@gmail.com>
2026-09-21 13:48:54 +0200

    feat: add my face

--- front/app/(public)/contacto/page.tsx
+++ front/app/(public)/contacto/page.tsx
@@ -2,6 +2,7 @@ import { jetBrain } from "@/app/fonts/fonts";
 import { BackgroundDecor } from "@/components/elements/background-decorator";
 import MyName from "@/components/elements/my-name";
 import { CustomLink } from "@/components/link/customLink";
+import Image from "next/image";
 
 export default function Contacto() {
     return (
@@ -9,8 +10,15 @@ export default function Contacto() {
             <p className={`${jetBrain.className}`}><span className="text-primary">{'_>'}</span> whoami</p>
             <MyName />
             <div className="flex my-4">
-                <div className="flex-1">
-
+                <div className="flex-1 flex justify-center items-start">
+                    <div className="relative w-40 h-40 rounded-full overflow-hidden border">
+                        <Image
+                            src={"/yo.jpeg"}
+                            alt="Aleksander Trujillo Prokhorenko"
+                            fill
+                            className="object-cover"
+                        />
+                    </div>
                 </div>
                 <div className="flex-1 flex flex-col gap-3">
                     <p className="text-pretty">
@@ -64,7 +72,7 @@ export default function Contacto() {
                             <path fill="currentColor" d="m18.73 5.41l-1.28 1L12 10.46L6.55 6.37l-1.28-1A2 2 0 0 0 2 7.05v11.59A1.36 1.36 0 0 0 3.36 20h3.19v-7.72L12 16.37l5.45-4.09V20h3.19A1.36 1.36 0 0 0 22 18.64V7.05a2 2 0 0 0-3.27-1.64" />
                         </svg>
                     </CustomLink>
-                    <CustomLink href="https://nextjs.org" className="text-3xl hover:text-primary transition-colors">
+                    <CustomLink href="https://aprokhorenko.dev" className="text-3xl hover:text-primary transition-colors">
                         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 2048 2048">
                             <path d="M0 0h2048v2048H0z" fill="none" />
                             <path fill="currentColor" d="M1024 0q141 0 272 36t245 103t207 160t160 208t103 245t37 272q0 141-36 272t-103 245t-160 207t-208 160t-245 103t-272 37q-141 0-272-36t-245-103t-207-160t-160-208t-103-244t-37-273q0-141 36-272t103-245t160-207t208-160T751 37t273-37m0 1920q123 0 237-32t214-90t182-141t140-181t91-214t32-238q0-123-32-237t-90-214t-141-182t-181-140t-214-91t-238-32q-123 0-237 32t-214 90t-182 141t-140 181t-91 214t-32 238q0 123 32 237t90 214t141 182t181 140t214 91t238 32m597-880l48-144h75l-85 256h-75l-48-144l-48 144h-75l-85-256h75l48 144l48-144h74zm-464-144h75l-85 256h-75l-48-144l-48 144h-75l-85-256h75l48 144l48-144h74l48 144zm-512 0h75l-85 256h-75l-48-144l-48 144h-75l-85-256h75l48 144l48-144h74l48 144z" />
